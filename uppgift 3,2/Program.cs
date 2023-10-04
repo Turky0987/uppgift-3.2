be4416081fad7ad0e43e407425d413a19aa244a0
Tareq Bilal Al-Asaad <taal04003@utb.vaxjo.se>
2023-10-04 10:50:25 +0200

    Add project files.

---
+++ uppgift 3,2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Har du gått ut gymnasiet? (j/n): ");
+        char harGåtUtGymnasiet = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (harGåtUtGymnasiet == 'j' || harGåtUtGymnasiet == 'J')
+        {
+            Console.Write("Hur gammal är du? Ange ålder: ");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 22)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig.");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Vi letar tyvärr efter annan personal just nu.");
+        }
+    }
+}
